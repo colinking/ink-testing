9627da160f8b8baecc2dfd77b06d3f884c394e6e
Colin <me@colinking.co>
2019-10-18 16:30:57 -0700

    add analytics example

---
+++ src/analytics-await.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { render, Text, Box } from 'ink'
+import Analytics from 'analytics-node'
+
+/**
+ * Seems like it is exiting without awaiting for the promise around analytics-node.
+ * 
+ * Output:
+ *  Hello World!
+ *  [cli]: opening promise to send analytics
+ *
+ * Expected:
+ *  Hello World!
+ *  [cli]: opening promise to send analytics
+ *  [cli]: track call callback fired
+ *  [cli]: promise awaited; closing
+ */
+
+const Example: React.FC = () => {
+  return <Text>Hello World!{'\n'}</Text>
+}
+
+async function run() {
+  const { waitUntilExit } = render(<Example />, {
+    debug: process.env.DEBUG === 'true'
+  })
+  await waitUntilExit()
+
+  // Fire some analytics.
+  const analyticsNode = new Analytics('123456789', {
+    flushAt: 1
+  })
+
+  console.log('[cli]: opening promise to send analytics')
+
+  await new Promise(resolve => {
+    analyticsNode.track({
+      event: 'Command Run',
+      anonymousId: 'tbd',
+    }, () => {
+      console.log('[cli]: track call callback fired')
+      resolve()
+    })
+  })
+  
+  console.log('[cli]: promise awaited; closing')
+}
+
+run()
